ed105cbfd6b20e724401b94fe427449c95ef0406
Zelles Tamás <zelles.tamas@gmail.com>
2020-11-10 07:13:29 +0100

    add Fourth_lesson_empty.py

---
+++ Dolgozat/rendszam.py
@@ -0,0 +1,9 @@
+
+
+bekeres = True
+vege = not True
+while bekeres != vege :
+    rendszam = (input("Kérem adjon meg egy rendszámot(AAA-111 formátumban): "))
+    leallit = str("Vége")
+    if rendszam == leallit :
+       vege = True
---
+++ Fourth_lesson_empty.py
@@ -0,0 +1,68 @@
+# Ötödik óra 2020.11.10.
+
+# Összetett adattípusok
+
+# Tuple
+# A tuple egy számokkal indexelhető, nem módosítható lista. Gyakorlatilag hasonló jelenség mint a string, csak itt nem karktereken
+# tudunk végig haladni, hanem a tömb egyes elemein. Az indexelés 0-tól kezdödik, és az elemszám-1 ig tart. Az elemeknek nem kell
+# azonos típusuaknak lennie, tehát lehet az, hogy három darab egész szám és egy string van tárolva egy tupleön belül.
+
+# 1a Hozzunk létre egy t nevű tuple változót, ami 3 db egész számot tartalmaz, majd irassuk ki.
+print ('1a')
+
+# 1b Határozzuk meg a típust, és irassuk ki a képernyőre.
+print ('\n1b')
+
+# 1c Írassuk ki a képernyőre a tuple elemeinek számát.
+print ('\n1c')
+
+# 1d Írassuk ki a tuple második elemét.
+print ('\n1d')
+
+# 1e Próbáljuk ki mi történik túlindexelés esetén.
+print ('\n1e')
+
+# 1f Módosítsuk az első értéket 100-ról 10-re
+print ('\n1f')
+
+# 1g Tároljunk el egy t nevű változóba három számot és egy stringet
+print ('\n1g')
+
+# 1h Hozzon létre egy gyümölcsöket tartalmazó fruits nevű tuple változót, majd ellenőrizze, hogy kérjen be a felhasználotól egy
+# gyümölcsöt, és ellenőrizze tartalmazza e a tuple.
+print ('\n1h')
+
+# 1i Hozzon létre egy üres tuple változót aminek legyen empty a neve, és irassa ki típusát
+print ('\n1i')
+
+# 1j A just_one_element változóban tárolja el a 42 es értéket.
+print ('\n1j')
+
+# List
+# A lista a tuple módosítható változata. Tudunk új element hozzáadni, valamint az indexelt elemeket módosítni, szintén nincs
+# megkötés az elemek típusára.
+
+# 2a Hozzunk lére egy lista nevű listaváltozót és tároljuk el benne négy egész számot. Írassuk ki a listát, és a típust
+print ('\n2a')
+
+# 2b Írassa ki a listaváltozó negyedik elemét, majd módosítsa 5-re és irassa ki a teljes listát
+print ('\n2b')
+
+# 2c Hozzunk létre egy citys nevű változót, tároljunk el benne 4 várost, írassuk is ki. Kérjen be a felhasználotól ezek után
+# egy várost, majd adja hozzá a listához és írassa ki.
+print ('\n2c')
+
+# 2d Szúrjunk be a city listának harmadik helyére még egy város nevét. Majd írassuk ki a megváltozott lista tartalmát
+print ('\n2d')
+
+# 2e Ellenőrizzük, hogy a lista tartalmazza e Budapestet.
+print ('\n2e')
+
+# 2f Töröljük kia a lista harmadik elemét, viszont jelenítsük meg a törölt elemet, és az új listát is.
+print ('\n2f')
+
+# 2e Töröljük ki a lista utolsó elemét, majd írjuk ki a listát
+print ('\n2e')
+
+# 2f Hozzunk létre egy nums nevű üres változót, majd töltsük fel 1- től 10-ig az egész számokkal.
+print ('\n2f')
